c154574c743f31e3d53b6f4ec669509a166789bb
Alpenschilling <alpenschilling@gmail.com>
2018-04-02 01:26:30 -0700

    Update ONE

---
+++ README.md
@@ -0,0 +1,4 @@
+# Readme
+
+This is Alpenschilling.
+Decentralized platform for sports and leisure activities in alpine regions.
---
+++ src/drafted/governance-categories.h
@@ -0,0 +1,58 @@
+
+/*
+	CATEGORY MAPPING
+
+	* means the category has an associated class
+	
+	CAlpsNetwork: ALPS NETWORK (ROOT)
+	has:
+		vector<CNetworkVariable> vecNetworkVariables;
+		vector<CAlpsProject> vecProjects;
+		vector<CGovernanceObject> vecProposals;
+		vector<CBudgetContract> vecContracts;
+		vector<CBudgetUsers> vecUsers;
+
+	CAlpsProject:
+	has:
+		name
+		employees
+
+*/
+
+	// ALPS NETWORK (ROOT)
+	// 	-> NETWORK VARIABLE
+	// 		-> switch, setting
+	// 	-> CATEGORIES
+	// 		-> LEVEL
+	// 			-> I, II, III, IV, V, VI, VII, VIII, IX, X, XI
+	// 		-> VALUEOVERRIDE
+	// 			-> NETWORK, OWNER 
+	// 		-> PROJECT*
+	// 			-> TYPES
+	// 				-> SOFTWARE
+	// 					-> CORE, NONCORE
+	// 				-> HARDWARE
+	// 				-> PR
+	// 			-> PROJECT REPORT*
+	// 				-> UPDATE
+	// 			-> PROJECT MILESTONE*
+	// 				-> START, ONGOING, COMPLETE, FAILURE
+	// 			-> PROPOSAL*
+	// 				-> FUNDING, GOVERNANCE, AMEND, GENERIC
+	// 			-> CONTRACT*
+	// 				-> TYPE
+	// 					-> INTERNAL, EXTERNAL
+	// 				-> STATUS
+	// 					-> OK
+	// 	-> GROUPS
+	// 		-> GROUP1
+	// 			-> USER1 (only users are allowed here in this scope)
+	// 			-> USER2
+	// 		-> GROUP2 (EVO)
+	// 			-> VALUEOVERRIDE (STORE=ALPSDRIVE)
+	// 			-> USER1
+
+	// 	-> COMPANIES
+	// 		-> COMPANY1
+	// 		-> DAO1
+
